1dd883e6c9746743b5209f1ac53dace08257aa4b
Alicia <calderon@ifca.unican.es>
2018-03-12 15:16:43 +0100

    step 2 files

--- crab_cfg_step2.py
+++ crab_cfg_step2.py
@@ -1,24 +1,25 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'Higgs_Zp2HDM_ww_MZP2500_MA0300_13TeV_RECO'
+config.General.requestName = 'ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-600_MA0-300_Mchi1-1_ctau-1_13TeV-madgraph_AODSIM'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 #config.JobType.generator = 'lhe'
-config.JobType.psetName = 'step2_25ns.py'
+config.JobType.psetName = 'EXO-RunIISummer16DR80Premix-01695_step2_cfg.py'
+#config.JobType.numCores = 4
 config.Data.inputDBS = 'phys03'
-config.Data.ignoreLocality = True
-config.Site.whitelist = ["T2_CH*"]
+#config.Data.ignoreLocality = True
+#config.Site.whitelist = ["T2_CH*"]
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 10
 NJOBS = 10
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshww/calderon/' #'/store/user/dburns/' # or '/store/group/<subdir>'
 config.Data.publication = False
-config.Data.primaryDataset = 'Higgs_Zp2HDM_ww_MZP2500_MA0300_13TeV/15112015/MINIAODSIM'
+#config.Data.primaryDataset = 'Higgs_Zp2HDM_ww_MZP2500_MA0300_13TeV/15112015/MINIAODSIM'
 #config.Data.publishDataName = 'Higgs_hzpzp_nohdecay_ww_1000GeV_13TeV_RECO_v1'
-config.Data.userInputFiles = open('Higgs_Zp2HDM_ww_MZP2500_MA0300_13TeV.txt').readlines()
+config.Data.userInputFiles = open('EXO-RunIISummer16DR80Premix-01695_step2_RAW.txt').readlines()
 
 config.section_('User')
 config.section_('Site')
