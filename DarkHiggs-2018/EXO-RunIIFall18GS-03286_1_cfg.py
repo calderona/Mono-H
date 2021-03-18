f055f902705303058596523b114bf0eb75141a2d
calderon <calderon@cern.ch>
2021-03-18 10:15:22 +0100

    updated

--- DarkHiggs-2018/EXO-RunIIFall18GS-03286_1_cfg.py
+++ DarkHiggs-2018/EXO-RunIIFall18GS-03286_1_cfg.py
@@ -66,80 +66,83 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '102X_upgrade2018_realistic_v11', '')
 
 
-process.generator = cms.EDFilter("Pythia8GeneratorFilter",
-    RandomizedParameters = cms.VPSet(
-        cms.PSet(
-            ConfigDescription = cms.string('DarkHiggs_MonoHs_HsToWWTo2l2nu_mhs_160_mx_100_mZp_195_TuneCP5_13TeV'),
-            ConfigWeight = cms.double(0.0112359550562),
-            GridpackPath = cms.string('/cvmfs/cms.cern.ch/phys_generator/gridpacks/2017/13TeV/madgraph/V5_2.6.5/DarkHiggs/DarkHiggs_WW/HsToWWTo2l2nu/DarkHiggs_MonoHs_HsToWWTo2l2nu_mhs_160_mx_100_mZp_195_TuneCP5_13TeV_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz'),
-            PythiaParameters = cms.PSet(
-                parameterSets = cms.vstring(
-                    'pythia8CommonSettings', 
-                    'pythia8CP5Settings', 
-                    'pythia8PSweightsSettings', 
-                    'processParameters'
-                ),
-                processParameters = cms.vstring(
-                    'Main:timesAllowErrors    = 10000', 
-                    'ParticleDecays:limitTau0 = on', 
-                    'ParticleDecays:tauMax = 10'
-                ),
-                pythia8CP5Settings = cms.vstring(
-                    'Tune:pp 14', 
-                    'Tune:ee 7', 
-                    'MultipartonInteractions:ecmPow=0.03344', 
-                    'PDF:pSet=20', 
-                    'MultipartonInteractions:bProfile=2', 
-                    'MultipartonInteractions:pT0Ref=1.41', 
-                    'MultipartonInteractions:coreRadius=0.7634', 
-                    'MultipartonInteractions:coreFraction=0.63', 
-                    'ColourReconnection:range=5.176', 
-                    'SigmaTotal:zeroAXB=off', 
-                    'SpaceShower:alphaSorder=2', 
-                    'SpaceShower:alphaSvalue=0.118', 
-                    'SigmaProcess:alphaSvalue=0.118', 
-                    'SigmaProcess:alphaSorder=2', 
-                    'MultipartonInteractions:alphaSvalue=0.118', 
-                    'MultipartonInteractions:alphaSorder=2', 
-                    'TimeShower:alphaSorder=2', 
-                    'TimeShower:alphaSvalue=0.118'
-                ),
-                pythia8CommonSettings = cms.vstring(
-                    'Tune:preferLHAPDF = 2', 
-                    'Main:timesAllowErrors = 10000', 
-                    'Check:epTolErr = 0.01', 
-                    'Beams:setProductionScalesFromLHEF = off', 
-                    'SLHA:keepSM = on', 
-                    'SLHA:minMassSM = 1000.', 
-                    'ParticleDecays:limitTau0 = on', 
-                    'ParticleDecays:tau0Max = 10', 
-                    'ParticleDecays:allowPhotonRadiation = on'
-                ),
-                pythia8PSweightsSettings = cms.vstring(
-                    'UncertaintyBands:doVariations = on', 
-                    'UncertaintyBands:List = {isrRedHi isr:muRfac=0.707,fsrRedHi fsr:muRfac=0.707,isrRedLo isr:muRfac=1.414,fsrRedLo fsr:muRfac=1.414,isrDefHi isr:muRfac=0.5,fsrDefHi fsr:muRfac=0.5,isrDefLo isr:muRfac=2.0,fsrDefLo fsr:muRfac=2.0,isrConHi isr:muRfac=0.25,fsrConHi fsr:muRfac=0.25,isrConLo isr:muRfac=4.0,fsrConLo fsr:muRfac=4.0,fsr_G2GG_muR_dn fsr:G2GG:muRfac=0.5,fsr_G2GG_muR_up fsr:G2GG:muRfac=2.0,fsr_G2QQ_muR_dn fsr:G2QQ:muRfac=0.5,fsr_G2QQ_muR_up fsr:G2QQ:muRfac=2.0,fsr_Q2QG_muR_dn fsr:Q2QG:muRfac=0.5,fsr_Q2QG_muR_up fsr:Q2QG:muRfac=2.0,fsr_X2XG_muR_dn fsr:X2XG:muRfac=0.5,fsr_X2XG_muR_up fsr:X2XG:muRfac=2.0,fsr_G2GG_cNS_dn fsr:G2GG:cNS=-2.0,fsr_G2GG_cNS_up fsr:G2GG:cNS=2.0,fsr_G2QQ_cNS_dn fsr:G2QQ:cNS=-2.0,fsr_G2QQ_cNS_up fsr:G2QQ:cNS=2.0,fsr_Q2QG_cNS_dn fsr:Q2QG:cNS=-2.0,fsr_Q2QG_cNS_up fsr:Q2QG:cNS=2.0,fsr_X2XG_cNS_dn fsr:X2XG:cNS=-2.0,fsr_X2XG_cNS_up fsr:X2XG:cNS=2.0,isr_G2GG_muR_dn isr:G2GG:muRfac=0.5,isr_G2GG_muR_up isr:G2GG:muRfac=2.0,isr_G2QQ_muR_dn isr:G2QQ:muRfac=0.5,isr_G2QQ_muR_up isr:G2QQ:muRfac=2.0,isr_Q2QG_muR_dn isr:Q2QG:muRfac=0.5,isr_Q2QG_muR_up isr:Q2QG:muRfac=2.0,isr_X2XG_muR_dn isr:X2XG:muRfac=0.5,isr_X2XG_muR_up isr:X2XG:muRfac=2.0,isr_G2GG_cNS_dn isr:G2GG:cNS=-2.0,isr_G2GG_cNS_up isr:G2GG:cNS=2.0,isr_G2QQ_cNS_dn isr:G2QQ:cNS=-2.0,isr_G2QQ_cNS_up isr:G2QQ:cNS=2.0,isr_Q2QG_cNS_dn isr:Q2QG:cNS=-2.0,isr_Q2QG_cNS_up isr:Q2QG:cNS=2.0,isr_X2XG_cNS_dn isr:X2XG:cNS=-2.0,isr_X2XG_cNS_up isr:X2XG:cNS=2.0}', 
-                    'UncertaintyBands:nFlavQ = 4', 
-                    'UncertaintyBands:MPIshowers = on', 
-                    'UncertaintyBands:overSampleFSR = 10.0', 
-                    'UncertaintyBands:overSampleISR = 10.0', 
-                    'UncertaintyBands:FSRpTmin2Fac = 20', 
-                    'UncertaintyBands:ISRpTmin2Fac = 1'
-                )
-            )
-        ) 
-    ),
+process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",                                                                 
+    nEvents = cms.untracked.uint32(1121),                                                                                           
+    outputFile = cms.string('cmsgrid_final.lhe'),                                                                                   
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh'),                               
+    numberOfParameters = cms.uint32(1),                                                                                             
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/2017/13TeV/madgraph/V5_2.6.5/DarkHiggs/DarkHiggs_WW/HsToWWTo2l2nu/DarkHiggs_MonoHs_HsToWWTo2l2nu_mhs_160_mx_100_mZp_2000_TuneCP5_13TeV_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz')           
+) 
 
-   comEnergy = cms.double(13000.0),
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    ExternalDecays = cms.PSet(
+        Tauola = cms.untracked.PSet(
+            UseTauolaPolarization = cms.bool(True),
+            InputCards = cms.PSet(
+                mdtau = cms.int32(0),
+                pjak2 = cms.int32(0),
+                pjak1 = cms.int32(0)
+            )
+        ),
+        parameterSets = cms.vstring('Tauola')
+    ),
+    UseExternalGenerators = cms.untracked.bool(True),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
     filterEfficiency = cms.untracked.double(1.0),
-    maxEventsToPrint = cms.untracked.int32(1),
     pythiaHepMCVerbosity = cms.untracked.bool(False),
-    pythiaPylistVerbosity = cms.untracked.int32(1)
+    comEnergy = cms.double(13000.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
+            'Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:keepSM = on', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on'),
+        pythia8CP5Settings = cms.vstring('Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:ecmPow=0.03344', 
+            'PDF:pSet=20', 
+            'MultipartonInteractions:bProfile=2', 
+            'MultipartonInteractions:pT0Ref=1.41', 
+            'MultipartonInteractions:coreRadius=0.7634', 
+            'MultipartonInteractions:coreFraction=0.63', 
+            'ColourReconnection:range=5.176', 
+            'SigmaTotal:zeroAXB=off', 
+            'SpaceShower:alphaSorder=2', 
+            'SpaceShower:alphaSvalue=0.118', 
+            'SigmaProcess:alphaSvalue=0.118', 
+            'SigmaProcess:alphaSorder=2', 
+            'MultipartonInteractions:alphaSvalue=0.118', 
+            'MultipartonInteractions:alphaSorder=2', 
+            'TimeShower:alphaSorder=2', 
+            'TimeShower:alphaSvalue=0.118'),
+        pythia8PSweightsSettings = cms.vstring('UncertaintyBands:doVariations = on', 
+            'UncertaintyBands:List = {isrRedHi isr:muRfac=0.707,fsrRedHi fsr:muRfac=0.707,isrRedLo isr:muRfac=1.414,fsrRedLo fsr:muRfac=1.414,isrDefHi isr:muRfac=0.5,fsrDefHi fsr:muRfac=0.5,isrDefLo isr:muRfac=2.0,fsrDefLo fsr:muRfac=2.0,isrConHi isr:muRfac=0.25,fsrConHi fsr:muRfac=0.25,isrConLo isr:muRfac=4.0,fsrConLo fsr:muRfac=4.0,fsr_G2GG_muR_dn fsr:G2GG:muRfac=0.5,fsr_G2GG_muR_up fsr:G2GG:muRfac=2.0,fsr_G2QQ_muR_dn fsr:G2QQ:muRfac=0.5,fsr_G2QQ_muR_up fsr:G2QQ:muRfac=2.0,fsr_Q2QG_muR_dn fsr:Q2QG:muRfac=0.5,fsr_Q2QG_muR_up fsr:Q2QG:muRfac=2.0,fsr_X2XG_muR_dn fsr:X2XG:muRfac=0.5,fsr_X2XG_muR_up fsr:X2XG:muRfac=2.0,fsr_G2GG_cNS_dn fsr:G2GG:cNS=-2.0,fsr_G2GG_cNS_up fsr:G2GG:cNS=2.0,fsr_G2QQ_cNS_dn fsr:G2QQ:cNS=-2.0,fsr_G2QQ_cNS_up fsr:G2QQ:cNS=2.0,fsr_Q2QG_cNS_dn fsr:Q2QG:cNS=-2.0,fsr_Q2QG_cNS_up fsr:Q2QG:cNS=2.0,fsr_X2XG_cNS_dn fsr:X2XG:cNS=-2.0,fsr_X2XG_cNS_up fsr:X2XG:cNS=2.0,isr_G2GG_muR_dn isr:G2GG:muRfac=0.5,isr_G2GG_muR_up isr:G2GG:muRfac=2.0,isr_G2QQ_muR_dn isr:G2QQ:muRfac=0.5,isr_G2QQ_muR_up isr:G2QQ:muRfac=2.0,isr_Q2QG_muR_dn isr:Q2QG:muRfac=0.5,isr_Q2QG_muR_up isr:Q2QG:muRfac=2.0,isr_X2XG_muR_dn isr:X2XG:muRfac=0.5,isr_X2XG_muR_up isr:X2XG:muRfac=2.0,isr_G2GG_cNS_dn isr:G2GG:cNS=-2.0,isr_G2GG_cNS_up isr:G2GG:cNS=2.0,isr_G2QQ_cNS_dn isr:G2QQ:cNS=-2.0,isr_G2QQ_cNS_up isr:G2QQ:cNS=2.0,isr_Q2QG_cNS_dn isr:Q2QG:cNS=-2.0,isr_Q2QG_cNS_up isr:Q2QG:cNS=2.0,isr_X2XG_cNS_dn isr:X2XG:cNS=-2.0,isr_X2XG_cNS_up isr:X2XG:cNS=2.0}', 
+            'UncertaintyBands:nFlavQ = 4', 
+            'UncertaintyBands:MPIshowers = on', 
+            'UncertaintyBands:overSampleFSR = 10.0', 
+            'UncertaintyBands:overSampleISR = 10.0', 
+            'UncertaintyBands:FSRpTmin2Fac = 20', 
+            'UncertaintyBands:ISRpTmin2Fac = 1'),
+        processParameters = cms.vstring('Main:timesAllowErrors    = 10000', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tauMax = 10'),
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+            'pythia8CP5Settings', 
+            'processParameters', 
+            'pythia8PSweightsSettings')
+    )
 )
 
 
 process.ProductionFilterSequence = cms.Sequence(process.generator)
 
 # Path and EndPath definitions
+process.lhe_step = cms.Path(process.externalLHEProducer)
 process.generation_step = cms.Path(process.pgen)
 process.simulation_step = cms.Path(process.psim)
 process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
@@ -147,11 +150,13 @@ process.endjob_step = cms.EndPath(process.endOfProcess)
 process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+process.schedule = cms.Schedule(process.lhe_step,process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
+
 # filter all path with the production filter sequence
 for path in process.paths:
+                                 if path in ['lhe_step']: continue
                                  getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
 
 # customisation of the process.
